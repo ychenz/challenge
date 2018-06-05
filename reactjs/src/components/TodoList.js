072f1e3cbc18052b7b38d85a5bb4f019a386390a
Yuchen Zhao <y376zhao@uwaterloo.ca>
2018-06-04 21:12:48 -0400

    Added react support

--- reactjs/src/components/TodoList.js
+++ reactjs/src/components/TodoList.js
@@ -2,9 +2,15 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class TodoList extends Component {
-    static propTypes = {
-        todos:PropTypes.array,
-    };
+    // static propTypes = {
+    //     todos:PropTypes.array,
+    // };
+
+    render(){
+        return (
+            <div>Hello world</div>
+        )
+    }
 }
 
 export default TodoList;
